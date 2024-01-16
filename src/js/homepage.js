85d2d5c992ed485a20b23e712d033a9b457c8963
Ashara Martinez <Asharamart2075@gmail.com>
2024-01-16 09:43:33 -0600

    changed position of the login page

--- src/js/homepage.js
+++ src/js/homepage.js
@@ -12,17 +12,23 @@ function Homepage() {
       <h1>Welcome to Staff Sphere</h1> 
       <div>
         <div>
-          <h2>Login:</h2>
+          <div className = "login"/>
+          <h2>Login</h2>
           <input type="text" placeholder="Enter your login" />
         </div>
-        <div>
-          <h2>Password:</h2>
+        <div className = "Password">
+          <h2>Password</h2>
           <input type={showPassword ? "text" : "password"} placeholder="Enter your password" />
           <button onClick={togglePasswordVisibility}>
             {showPassword ? "Hide" : "Show"} Password
           </button>
         </div>
+
+        <div className = "SignIn" >
         <button>Sign In</button>
+        </div>
+      </div>
+      <div className = "CreateLogin">
         <button>Create Login</button>
       </div>
     </div>
